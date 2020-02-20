9336c6db9becd402115ec3e789ac82db602989c8
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-20 18:53:19 +0530

    03. Simple routes

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 var micro = require("./lib/micro-express");
-
 var app = micro();
+
+app.get("/hello", function (req, res) {
+  let message = "Hello Micro Express :)";
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', message.length);
+  res.writeHead(200);
+  res.end(message);
+});
+
+app.get("/bye", function (req, res) {
+  let message = "Bye for now. See you agin :)";
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', message.length);
+  res.writeHead(200);
+  res.end(message);
+});
 
 app.listen(3000);
