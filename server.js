6552cd3ee1f99ef3771de3e8308a43acd5106140
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-20 19:18:08 +0530

    05. Request Object with parameters. Also added qs npm package

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var micro = require("./lib/micro-express");
 var app = micro();
 
 app.get("/hello", function (req, res) {
-  res.send("Hello Micro Express " + req.params["name"]);
+  http://localhost:3000/hello?user[name]=rajesh&user[city]=mumbai
+  res.send("Hello Micro Express " + req.params["user"]["name"] + " " + req.params["user"]["city"]);
 });
 
 app.get("/bye", function (req, res) {
