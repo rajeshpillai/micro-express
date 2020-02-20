8601b918ea307ce83bba706a137e19be3f7f9a07
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-20 18:26:31 +0530

    1. Simple Server

---
+++ server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+const PORT = process.env.PORT || 3000;
+var server = http.createServer(function (req, res) {
+  var message = "Hello HTTP";
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', message.length);
+  res.writeHead(200);
+  res.end(message);
+});
+
+server.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
