353c3eaf8328d405fd948c8a0c0de1fd43ea00fb
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-02-21 12:14:13 +0530

    07. Handle POST request

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 var micro = require("./lib/micro-express");
 var app = micro();
 
+// POST support
+app.get("/post", function (req, res) {
+  res.html(`
+    <form method="POST">
+      <input type="text" name="username" placeholder="enter username" />
+      <button type="submit">submit</button>
+    </form>
+  `);
+});
+
+app.post("/post", function (req, res) {
+  res.send("OK : " + req.body);
+});
+
 // Add dynamic parameter
 app.get("/ping/:ip", function (req, res) {
   res.send(`Pinging ${req.params.ip}`);
